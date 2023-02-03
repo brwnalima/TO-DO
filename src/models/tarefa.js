f5f71d62516cd22b475a589cc87d080c2ad0c5ec
brwnalima <bruna.lima@edu.unirio.br>
2023-02-03 09:02:59 -0300

    models

---
+++ src/models/tarefa.js
@@ -0,0 +1,9 @@
+class Tarefa {
+    constructor (titulo, descrição, status) {
+        this.titulo = titulo
+        this.descrição = descrição
+        this.status = status
+    }
+}
+
+export default Tarefa
