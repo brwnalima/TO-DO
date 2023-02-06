8c1417a01d7a9763536def78a2d36711c78ebfa6
brwnalima <bruna.lima@edu.unirio.br>
2023-02-06 10:33:55 -0300

    Update usuario-controller.js

--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -1,10 +1,11 @@
-// import { bdUsuarios } from '../bd.js';
+import { bdUsuarios } from '../bd.js';
 
 class usuarioController {
     static rotas(app) {
          // Rota para o recurso usuario
          app.get('/', usuarioController.listar)
          app.post('/', usuarioController.inserir)
+         app.get('/email/:email', usuarioController.buscarPorEmail)
          // /usuario
     }
 
@@ -20,7 +21,18 @@ class usuarioController {
         console.log(req.body)        
     }
 
-                                                               
+    static buscarPorEmail(req, res) {
+        // busca o email na lista de usuarios
+        const usuario = bdUsuarios.find(usuario => usuario.email === req.params.email)
+
+        // se o usuario n for encontrado, devolve um erro
+        if (!usuario) {
+            res.status(404).send("Usuário não encontrado!")
+        }
+        
+        // se o usuario for encontrado, devolve o usuario
+        res.send(usuario)
+    }                                    
 }
 
 export default usuarioController
