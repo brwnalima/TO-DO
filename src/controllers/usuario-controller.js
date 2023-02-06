ea07abbbe825c7755304b4f25b44732f6117e6f1
brwnalima <bruna.lima@edu.unirio.br>
2023-02-06 10:39:21 -0300

    rota email em usuario

--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -2,11 +2,11 @@ import { bdUsuarios } from '../bd.js';
 
 class usuarioController {
     static rotas(app) {
-         // Rota para o recurso usuario
-         app.get('/', usuarioController.listar)
-         app.post('/', usuarioController.inserir)
-         app.get('/email/:email', usuarioController.buscarPorEmail)
-         // /usuario
+        // Rota para o recurso usuario
+        app.get('/', usuarioController.listar)
+        app.post('/', usuarioController.inserir)
+        app.get('/email/:email', usuarioController.buscarPorEmail)
+        // /usuario
     }
 
     static listar(req, res) {
@@ -15,10 +15,10 @@ class usuarioController {
         res.send(usuario)
     }
 
-    static inserir(req, res){
+    static inserir(req, res) {
         res.send('Rota ativada com POST e recurso usuario: usuario deve ser inserido')
         // Console log do corpo da requisição
-        console.log(req.body)        
+        console.log(req.body)
     }
 
     static buscarPorEmail(req, res) {
@@ -28,11 +28,13 @@ class usuarioController {
         // se o usuario n for encontrado, devolve um erro
         if (!usuario) {
             res.status(404).send("Usuário não encontrado!")
+        } else {
+            // se o usuario for encontrado, devolve o usuario
+            res.send(usuario)
         }
-        
-        // se o usuario for encontrado, devolve o usuario
-        res.send(usuario)
-    }                                    
+
+
+    }
 }
 
 export default usuarioController
